e2574aa9f4863c3a55ec711fa5833555aab4ee37
ms-poojary <varshinipoojary04@gmail.com>
2024-11-08 19:25:57 +0530

    updated tabs

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,18 @@
-import { Text, View } from "react-native";
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
 
-export default function Index() {
+const index = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text>index</Text>
     </View>
-  );
+  )
 }
+
+const styles=StyleSheet.create({
+    container:{
+        alignContent:'center'
+    }
+})
+
+export default index
